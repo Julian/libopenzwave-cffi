6d53dd07a03c4bd24b9ca75cbbfe1c9fc3d4ff1f
Julian Berman <Julian@GrayVines.com>
2016-03-10 20:51:43 -0500

    Add back the C++ extern

--- cpp/libopenzwavec.h
+++ cpp/libopenzwavec.h
@@ -1,2 +1,8 @@
-typedef void* COptions;
-COptions newCOptions(const char*, const char*, const char*);
+#ifdef __cplusplus
+extern "C" {
+#endif
+    typedef void* COptions;
+    COptions newCOptions(const char*, const char*, const char*);
+#ifdef __cplusplus
+}
+#endif
